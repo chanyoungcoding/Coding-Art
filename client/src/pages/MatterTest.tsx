39d21d9c2b0661d989e6f516e039fc2e4e411ca7
chanyoungcoding <white1614@naver.com>
2024-03-10 13:14:10 +0900

    matter 수정

--- client/src/pages/MatterTest.tsx
+++ client/src/pages/MatterTest.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import { Engine, Render, Runner, Bodies, World } from 'matter-js';
+import Matter from 'matter-js';
 
 const MatterTest: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -17,33 +18,29 @@ const MatterTest: React.FC = () => {
     });
     const runner = Runner.create();
     
-    // 세모
-    const triangle = Bodies.polygon(400, 200, 3, 80, { 
-      render: { fillStyle: '#0000ff' } // 파랑색으로 채우기
-    });
-
-    // 세모-2 x, y, 변, 크기
-    const triangle2 = Bodies.polygon(700, 200, 3, 100, { 
-      render: { fillStyle: '#7272ff' } // 파랑색으로 채우기
-    });
-  
-
-    // 동그라미
-    const circle = Bodies.circle(450, 50, 40, { 
-      render: { fillStyle: '#ff0000' } // 빨강색으로 채우기
-    });
-
-    // 네모
-    const rectangle = Bodies.rectangle(500, 70, 80, 80, { 
-      render: { fillStyle: '#4cc54c' } // 초록색으로 채우기
-    });
-    
+    const triangle = Bodies.polygon(400, 200, 3, 80, { render: { fillStyle: '#0000ff' } });
+    const triangle2 = Bodies.polygon(700, 200, 3, 100, { render: { fillStyle: '#7272ff' } });
+    const circle = Bodies.circle(450, 50, 40, { render: { fillStyle: '#ff0000' } });
+    const rectangle = Bodies.rectangle(500, 70, 80, 80, { render: { fillStyle: '#4cc54c' } });
     const ground = Bodies.rectangle(450, 610, 810, 60, { isStatic: true });
 
     World.add(engine.world, [triangle, triangle2, circle, rectangle, ground]);
 
     Runner.run(runner, engine);
     Render.run(render);
+
+    const handleCanvasClick = (event: MouseEvent) => {
+      const mouseX = event.clientX - canvasRef.current!.getBoundingClientRect().left;
+      const mouseY = event.clientY - canvasRef.current!.getBoundingClientRect().top;
+
+      const bodiesUnderMouse = Matter.Query.point(engine.world.bodies, { x: mouseX, y: mouseY });
+
+      bodiesUnderMouse.forEach(body => {
+        console.log('클릭된 바디:', body);
+      });
+    };
+
+    canvasRef.current?.addEventListener('click', handleCanvasClick);
 
     return () => {
       Render.stop(render);
